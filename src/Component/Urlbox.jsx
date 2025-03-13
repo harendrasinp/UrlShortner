f309a56a79b539e81e78b737ec702aaf5bb8f668
Harendrasinh <haren111990@gmail.com>
2025-03-13 12:08:25 +0530

    update http in short url link

--- src/Component/Urlbox.jsx
+++ src/Component/Urlbox.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { localStorageThunk, fetchDataFromLocalStorage } from '../reducer/Urlreducer'
+import { Link } from 'react-router-dom';
 export const Urlbox = () => {
   const [url, setUrl] = useState("");
 
@@ -24,7 +25,7 @@ export const Urlbox = () => {
       </div>
       {urlData.length > 0 ? urlData.map((urls, index) => (
         <div key={index} className='flex justify-around items-center flex-col'>
-          <div className='text-amber-100 text-[1.2rem] rounded-[0.2rem] bg-blue-200/35  w-[20rem] mb-3 flex justify-center items-center'><a href={urls.shorturl}>http://localhost:3000/{urls.shorturl}</a></div>
+          <div className='text-amber-100 text-[1.2rem] rounded-[0.2rem] bg-blue-200/35  w-[20rem] mb-3 flex justify-center items-center'><Link to={`http://localhost:3000/${urls.shorturl}`}>http://localhost:3000/{urls.shorturl}</Link></div>
         </div>
       )) : null}
     </div>
