bad057bc8b02b728cd7f9c2d45e6e031e4e53f15
Harendrasinh <haren111990@gmail.com>
2025-03-13 23:19:34 +0530

    update footer data

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer className='w-full h-[4rem] mt-[9rem] text-amber-50 flex justify-center items-center flex-col' >
+        <footer className='absolute top-[33.4rem] lg:top-[29.9rem] w-full h-[8rem] bg-blue-200/20 text-amber-50 flex justify-center items-center flex-col' >
             <div>© 2025 URL Shortener</div>
-            <div>Developed by <strong>Vayu Softwares Team</strong></div>
+            <div>Developed by <strong>VayuSoftwares Team</strong></div>
             <div>Founder <strong>Rajendrasinh Parmar</strong></div> 
             <div>Co-Founder <strong>Harendrasinh Parmar</strong></div>
         </footer>
