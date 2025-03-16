0105dc77fd529ff24a8ffdc59d6e5b2f5d1e0fbd
Harendrasinh <haren111990@gmail.com>
2025-03-16 19:52:39 +0530

    footer change

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className=' w-full h-[8rem] bg-blue-200/20 text-amber-50 flex justify-center items-center flex-col lg:top-[29.9rem]' >
             <div>© 2025 URL Shortener</div>
             <div>Developed by <strong>VayuSoftwares Team</strong></div>
-            <div>Founder <strong>Rajendrasinh Parmar</strong></div> 
+            <div>Founder <strong>Sneha Harendrasinh Parmar</strong></div> 
             <div>Co-Founder <strong>Harendrasinh Parmar</strong></div>
         </footer>
     )
