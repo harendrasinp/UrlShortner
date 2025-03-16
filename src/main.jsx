027c03f718c62de1bb634a5d8d30f578ebcb2352
Harendrasinh <haren111990@gmail.com>
2025-03-16 19:41:53 +0530

    image added for app

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,12 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Navbar/>
-        <Home/>
-        <App/>
+        <Navbar />
+        <div className='absolute mt-[5rem] z-10 w-full h-[12rem] flex justify-center items-center lg:-mt-[2rem]'><img className='w-[5rem] h-[5rem]' src="https://cdn-icons-png.flaticon.com/128/15532/15532705.png" alt="car" /></div>
+        <Home />
+        <App />
         <div className='fixed bottom-0 w-full'>
-          <Footer/>
+          <Footer />
         </div>
       </BrowserRouter>
     </Provider>
