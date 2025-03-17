427f04717e61a407f37d55734c6e7838f6d30f0a
Harendrasinh <haren111990@gmail.com>
2025-03-17 19:46:20 +0530

    add margin to home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export const Home = () => {
 
   return (
-    <div className='lg:hidden flex justify-center items-center  lg:mt-0'>
+    <div className='lg:hidden flex justify-center items-center mb-6'>
       <div className='flex justify-around w-[25rem] text-amber-50'>
         <div className='bg-cyan-700 w-fit p-2 rounded-md text-[1.2rem] transition active:scale-105'><Link to={"/"}>Home</Link></div>
         <div className='bg-cyan-700 w-fit p-2 rounded-md text-[1.2rem] transition active:scale-105'><Link to={"/ulrShortner"}>Url Shortner</Link></div>
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <Navbar />
-        <div className='mt-[2rem] z-10 w-full flex justify-center items-center'><img className='w-[6rem]' src="https://cdn-icons-png.flaticon.com/128/15532/15532705.png" alt="car" /></div>
+        <div className='z-10 w-full flex justify-center items-center'><img className='w-[6rem]' src="https://cdn-icons-png.flaticon.com/128/15532/15532705.png" alt="car" /></div>
         <Home />
         <App />
         <Footer />
