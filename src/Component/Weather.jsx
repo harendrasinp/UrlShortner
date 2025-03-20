8ef9b404419297d006d38862272068f6438e3393
Harendrasinh <haren111990@gmail.com>
2025-03-20 13:27:17 +0530

    Weather page mesage add for contruction

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Weather = () => {
   return (
-    <div>Weather</div>
+    <div className='text-red-600 font-bold text-[0.9rem] flex justify-center items-center'>This Page is in Under Construction Mode</div>
   )
 }
