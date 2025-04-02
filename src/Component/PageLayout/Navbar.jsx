e4e289de85afe3d118cc78581ba77cc53432b7cc
Harendrasinh <haren111990@gmail.com>
2025-04-02 21:29:02 +0530

    set title

--- src/Component/PageLayout/Navbar.jsx
+++ src/Component/PageLayout/Navbar.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MobNav } from './MobNav';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setMenu } from '../../reducer/mobileReducer';
 export const Navbar = () => {
-  const {menu} = useSelector((state) => state.mobile)
-  const dispatch=useDispatch();
+  const { menu } = useSelector((state) => state.mobile)
+  const dispatch = useDispatch();
   console.log(menu)
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <div className="flex justify-center items-center w-full h-[4rem] bg-slate-950/80  lg:flex lg:justify-between">
-       
-        <div className="text-amber-50 font-semibold text-[1.4rem] m-auto lg:ml-[5rem]">SmartTools</div>
-        
-        {menu?null:<img className='lg:hidden w-[3rem] h-[2.5rem]'onClick={()=>dispatch(setMenu(true))} src="https://cdn-icons-png.flaticon.com/128/10233/10233705.png" alt="three line"/>
-        }
+    <div className='flex flex-col justify-center'>
+      <div className="flex justify-center items-center w-full h-[4rem] bg-slate-950/80 lg:flex lg:justify-between">
+
+        <div className="text-amber-50 font-semibold w-[4rem] text-[1.4rem] ml-auto lg:ml-[5rem]">SmartTools</div>
+
+        <div className='ml-auto w-[3rem]'>
+          {menu ? null : <img className='lg:hidden w-[3rem] h-[2.5rem]' onClick={() => dispatch(setMenu(true))} src="https://cdn-icons-png.flaticon.com/128/10233/10233705.png" alt="three line" />
+          }
+        </div>
         <div className='hidden lg:w-[18rem] lg:flex justify-between mr-[5rem]'>
           <div className='text-amber-50'><Link to={"/"}>Home</Link></div>
           <div className='text-amber-50'><Link to={"/ulrShortner"}>Url Shortner</Link></div>
