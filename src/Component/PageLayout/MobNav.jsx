9f2808a08cf28c110f1aff755916fc9c00c7b875
Harendrasinh <haren111990@gmail.com>
2025-04-02 14:32:52 +0530

    space removed

--- src/Component/PageLayout/MobNav.jsx
+++ src/Component/PageLayout/MobNav.jsx
@@ -4,7 +4,7 @@ export const MobNav = () => {
 
   return (
     <div className='lg:hidden flex justify-center flex-col items-center mt-5'>
-      <div className='flex justify-around w-[25rem] text-amber-50'>
+      <div className='flex justify-around w-full text-amber-50'>
         <div className='bg-cyan-700 w-fit p-2 rounded-md text-[1.2rem] transition active:scale-105'><Link to={"/"}>Home</Link></div>
         <div className='bg-cyan-700 w-fit p-2 rounded-md text-[1.2rem] transition active:scale-105'><Link to={"/ulrShortner"}>Url Shortner</Link></div>
         <div className='bg-cyan-700 w-fit p-2 rounded-md transition active:scale-105 text-[1.2rem]'><Link to={"/weather"}>Weather</Link></div>
