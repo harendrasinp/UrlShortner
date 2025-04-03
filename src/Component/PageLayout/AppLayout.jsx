924aff4150b6f2eda8d6f27d94396df06d977762
Harendrasinh <haren111990@gmail.com>
2025-04-03 16:04:16 +0530

    add commit

--- src/Component/PageLayout/AppLayout.jsx
+++ src/Component/PageLayout/AppLayout.jsx
@@ -11,7 +11,6 @@ export const AppLayout = () => {
         <div className='flex flex-col min-h-screen'>
             <Navbar />
             <div className='self-end'>
-                {/* {menu ? <MobNav /> : null} */}
                 <MobNav/>
             </div>
             <main className='flex-1'>
